f885fc1397f1abf87d1eaa5a5e5f3e88bf7c1a30
alienzj <alienchuj@gmail.com>
2020-09-10 20:32:19 +0800

    update

--- config.el
+++ config.el
@@ -694,34 +694,76 @@ Make sure to put cursor on date heading that contains list of urls."
 (setq org-static-blog-drafts-directory
       "~/documents/doraemon/org/blog/alienzj.github.io/drafts/")
 (setq org-static-blog-enable-tags t)
-(setq org-export-with-toc nit)
+(setq org-export-with-toc nil)
 (setq org-export-with-section-numbers nil)
+
+;; page-header
+;; page-preamble
+;; page-postamble
 
 (setq org-static-blog-page-header
       "<meta name=\"author\" content=\"Jie Zhu\">
 <meta name=\"referrer\" content=\"no-referrer\">
-<link href= \"style/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link href= \"styles/style.css\" rel=\"stylesheet\" type=\"text/css\" />
 <link rel=\"icon\" href=\"images/org_logo.png\">")
 
 (setq org-static-blog-page-preamble
       "<div class=\"header\">
-  <a href=\"https://alienzj.github.io\">ZJ Org Blog</a>
+  <a href=\"https://alienzj.github.io/\" class=\"logo\">ZJ Org Blog</a>
+  <br>
+    <a href=\"https://alienzj.github.io/archive\">Archive</a>
+    <a href=\"https://alienzj.github.io/tags\">Tags</a>
+    <a href=\"https://alienzj.github.io/rss.xml\">RSS</a>
+    <a href=\"https://alienzj.github.io/about\">About</a>
 </div>")
 
 (setq org-static-blog-page-postamble
-      (concat
-       "<div id=\"archive\">"
-       "<a href=\"https://alienzj.github.io/archive.html\">Other posts</a>"
-       "</div>"
-       "<center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">"
-       "<img alt=\"Creative Commons License\" style=\"border-width:0\" "
-       "src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br />"
-       "<span xmlns:dct=\"https://purl.org/dc/terms/\" "
-       "href=\"https://purl.org/dc/dcmitype/Text\" "
-       "property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by "
-       "<a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://bastibe.de\" "
-       "property=\"cc:attributionName\" rel=\"cc:attributionURL\">"
-       "Bastian Bechtold</a> is licensed under a <a rel=\"license\" "
-       "href=\"https://creativecommons.org/licenses/by-sa/3.0/\">"
-       "Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>")
-      )
+      (s-collapse-whitespace (s-replace "\n" ""
+                                        "
+<center>
+  <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">
+     <img alt=\"Creative Commons License\" style=\"border-width:0\"
+          src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\"/>
+  </a>
+
+  <br />
+  <span xmlns:dct=\"https://purl.org/dc/terms/\"
+       href=\"https://purl.org/dc/dcmitype/Text\"
+       property=\"dct:title\" rel=\"dct:type\">
+       <em>ZJ Org Blog</em>
+  </span>
+
+  by
+  <a xmlns:cc=\"https://creativecommons.org/ns#\"
+  href=\"https://alienzj.github.io\"
+  property=\"cc:attributionName\" rel=\"cc:attributionURL\">
+    Jie Zhu
+ </a>
+
+ is licensed under a
+ <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">
+   Creative Commons Attribution-ShareAlike 3.0 Unported License
+</a>
+</center>
+
+
+<div id=\"disqus_thread\"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://https-alienzj-github-io.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>")))
