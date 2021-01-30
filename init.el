d0a7c9bd0e42c4cbca1f880bbc458eef0f4f3a46
alienzj <alienchuj@gmail.com>
2021-01-30 16:57:57 +0800

    update

--- init.el
+++ init.el
@@ -37,7 +37,9 @@
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;minimap
-       (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
+       (modeline
+        ;; +light
+        )          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink the current line after jumping
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
