cc1584629dc598c83ce2f2b42e359337228418f2
Jie Zhu <alienchuj@gmail.com>
2023-01-07 17:37:24 +0800

    update

--- modules/org/roam/config.el
+++ modules/org/roam/config.el
@@ -8,9 +8,9 @@
    org-roam-directory jack-org-dir
    org-roam-db-location (concat jack-org-dir ".org-roam.db")
 
-   org-roam-node-display-template
-   (concat "${title:*} "
-           (propertize "${tags:10}" 'face 'org-tag))
+   ;;org-roam-node-display-template
+   ;;(concat "${title:*} "
+   ;;        (propertize "${tags:10}" 'face 'org-tag))
 
    ;;* Foo
    ;;  :PROPERTIES:
@@ -24,16 +24,16 @@
   )
 
 (use-package! websocket
-    :after org-roam)
+  :after org-roam)
 
 (use-package! org-roam-ui
-    :after org-roam ;; or :after org
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
+  :after org-roam ;; or :after org
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
